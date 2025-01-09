0a1fd271d446e371ffbe839ce624acb1a4d079f2
neel_8800 <pneel8800@gmail.com>
2025-01-10 00:03:44 +0530

    Created express server + error handler + middleware

--- src/app.js
+++ src/app.js
@@ -1,1 +1,36 @@
-console.log("Starting of new DevTinder Project");
+const express = require("express");
+const { adminAuth, userAuth } = require("./middleware/auth");
+
+const app = express();
+
+app.use("/admin", adminAuth);
+
+app.get("/admin", (req, res) => {
+  res.status(200).send([
+    {
+      userName: "admin",
+      role: "admin",
+    },
+  ]);
+});
+
+app.post("/admin", (req, res) => {
+  res.status(201).send([
+    {
+      userName: "admin1",
+      role: "admin",
+    },
+  ]);
+});
+
+app.get("/user", userAuth, (req, res) => {
+  try {
+    throw new Error("User not found");
+  } catch (error) {
+    res.status(404).send(error.message);
+  }
+});
+
+app.listen(8888, () => {
+  console.log("DevTinder-Backend is running and listening on port 8888...");
+});
